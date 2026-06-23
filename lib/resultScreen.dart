b79c2a703b9da044349858765a8e18c56e3a28dc
Sharath44665 <sharathwelcomesyouin@gmail.com>
2026-06-23 12:22:42 +0530

    add implementation of result screen

--- lib/resultScreen.dart
+++ lib/resultScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/question.dart';
+import 'package:quiz_app/startScreen.dart';
 
 class ResultScreen extends StatelessWidget {
   final List<Question> questions;
@@ -13,6 +14,16 @@ class ResultScreen extends StatelessWidget {
     required this.userSelected,
   });
 
+  int get scoreData {
+    int score = 0;
+    for (var idx = 0; idx < questions.length; idx += 1) {
+      if (userSelected[idx] == questions[idx].correctIndex) {
+        score += 1;
+      }
+    }
+    return score;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,27 +33,36 @@ class ResultScreen extends StatelessWidget {
         child: Column(
           children: [
             Text(
-              "You Scored $score out of $qLength",
+              "You Scored $scoreData out of ${questions.length}",
               style: TextStyle(fontSize: 22),
             ),
             const SizedBox(height: 12),
             Expanded(
               child: ListView.builder(
-                itemCount: 1,
-                itemBuilder: (context, index) {
+                itemCount: questions.length,
+                itemBuilder: (_, index) {
+                  final questionData = questions[index];
+                  final selected = userSelected[index];
+                  final correct = questionData.correctIndex;
                   return Card(
                     child: ListTile(
-                      title: Text("some question data"),
+                      title: Text(questionData.question),
                       subtitle: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           const SizedBox(height: 8),
                           Text(
-                            "your Answer: you selected/not answered/change color",
-                            style: TextStyle(color: Colors.blue.shade700),
+                            "your Answer: ${selected == -1 ? 'No Answer' : questionData.options[selected]}",
+                            style: TextStyle(
+                              color: selected == correct
+                                  ? Colors.green
+                                  : Colors.red,
+                            ),
                           ),
-                          Text("display correct answer"),
-                          Text("show explanation"),
+                          Text(
+                            "Correct answer: ${questionData.options[correct]}",
+                          ),
+                          Text("Explanation: ${questionData.explanation}"),
                         ],
                       ),
                     ),
@@ -51,7 +71,12 @@ class ResultScreen extends StatelessWidget {
               ),
             ),
             ElevatedButton.icon(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (_) => const StartScreen()),
+                );
+              },
               label: Text("Restart"),
               icon: Icon(Icons.restart_alt),
             ),
